4abf1c5087e53d08c54637798dbb44859a12ec7d
Jynxer <jordanrowley@mail.com>
2025-05-15 18:58:31 +0100

    Removed transparent background and telescope plugin

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "NONE", fg = "#444444" })
+-- vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "NONE", fg = "#444444" })
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,6 +11,6 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
     vim.opt_local.spell = false
-    vim.b.disable_autoformat = true
+    vim.b.autoformat = false
   end,
 })
