0dab7c7de6cb7f0b288a9d925fccc3f5c42d8c11
Jynxer <jordanrowley@mail.com>
2025-06-25 15:21:09 +0100

    Disabled tailwind colouriser for markdown files

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,14 +1,10 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    -- Other configuration options...
     enabled = function()
       local disabled = false
       -- Disable in Markdown files
       disabled = disabled or (vim.bo.filetype == "markdown")
-      -- Disable in prompt buffers
-      -- disaled = disabled or (vim.bo.buftype == "prompt")
-      -- Add any additional conditions here
       return not disabled
     end,
   },
--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -15,18 +15,23 @@ return {
       },
     },
   },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-    },
-    opts = function(_, opts)
-      -- original LazyVim kind icon formatter
-      local format_kinds = opts.formatting.format
-      opts.formatting.format = function(entry, item)
-        format_kinds(entry, item) -- add icons
-        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-      end
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   enabled = function()
+  --     local disabled = false
+  --     -- Disable in Markdown files
+  --     disabled = disabled or (vim.bo.filetype == "markdown")
+  --     return not disabled
+  --   end,
+  --   dependencies = {
+  --     { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+  --   },
+  --   opts = function(_, opts)
+  --     local format_kinds = opts.formatting.format
+  --     opts.formatting.format = function(entry, item)
+  --       format_kinds(entry, item) -- add icons
+  --       return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+  --     end
+  --   end,
+  -- },
 }
